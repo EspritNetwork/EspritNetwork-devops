fba9a1950efe44de6f2171108369f4dfd9f25743
mecherguieya <eyamechergui07@gmail.com>
2024-03-26 16:04:41 +0100

    testenv1

---
+++ test/affiliation.test.js
@@ -0,0 +1,34 @@
+require('dotenv').config({ path: '.env.test' });
+
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const Affiliation = require('../models/affiliation');
+
+const mongoose = require('mongoose');
+
+
+chai.use(chaiHttp);
+
+
+describe('Test', () => {
+
+
+it('should POST a valid product', (done) => {
+        
+    let affiliation = {
+        name: "Test Affiliation",
+    }
+    chai.request(server)
+    .post('/affiliation/add')
+    .send(affiliation)
+    .end((err, res) => {
+        res.should.have.status(201);
+        done();
+    });
+
+});
+
+});
