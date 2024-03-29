8c5d48a80d88ecac27f9e8f25c7a7df0f43a0a02
mecherguieya <eyamechergui07@gmail.com>
2024-03-29 02:56:11 +0100

    5008

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use("/departement", departementRouter);
 
 // Server configuration
 const server = http.createServer(app);
-const port = 5007;
+const port = 5008;
 server.listen(port, () => console.log(`Server listening on port ${port}`));
 
 module.exports = app;
--- dockerfile
+++ dockerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 COPY . /app
 RUN npm install
 RUN npm run build-dev
-EXPOSE 5007
+EXPOSE 5008
 CMD  ["npm", "start"]
